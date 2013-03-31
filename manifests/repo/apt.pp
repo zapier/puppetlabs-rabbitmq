fcf0d70318e51cbb6593ffab38437125eedfa7a2
James Carr <james@zapier.com>
2013-03-30 23:20:02 -0500

    merged various open PRs

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -5,24 +5,13 @@
 class rabbitmq::repo::apt(
   $pin = undef
 ) {
-
-  Class['rabbitmq::repo::apt'] -> Package<| title == 'rabbitmq-server' |>
-
   apt::source { 'rabbitmq':
     location    => 'http://www.rabbitmq.com/debian/',
     release     => 'testing',
     repos       => 'main',
     include_src => false,
     key         => 'RabbitMQ Release Signing Key <info@rabbitmq.com>',
-    key_content => template('rabbitmq/rabbit.pub.key'),
+    key_source  => 'http://www.rabbitmq.com/rabbitmq-signing-key-public.asc',
     pin         => $pin,
-  }
-
-  if ! ($pin == undef) {
-    validate_re($pin, '\d\d\d')
-    apt::pin { 'rabbitmq':
-      packages => 'rabbitmq-server',
-      priority => $pin,
-    }
   }
 }
